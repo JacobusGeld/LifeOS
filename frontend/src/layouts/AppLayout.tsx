371498efc1763498609eb7f42b8521a23c636e71
Jacobus Geldenhuys <Jgeldenhuys@live.com>
2026-08-03 10:39:56 +0200

    Updated protected route import type

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,11 +1,23 @@
 import { Outlet } from 'react-router-dom';
 
+import NavigationBar from '../components/navigation/NavigationBar';
+
+import appNavigation from '../app/navigation/appNavigation';
+
 function AppLayout() {
-   return (
-       <div>
-           <Outlet />
-       </div>
-   );
+    return (
+        <>
+            <header>
+                <h1>Life OS</h1>
+
+                <NavigationBar routes={appNavigation} />
+            </header>
+
+            <main>
+                <Outlet />
+            </main>
+        </>
+    );
 }
 
 export default AppLayout;
